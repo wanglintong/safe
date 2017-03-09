3a8ae1e227b8973f1d84cc634869df93768e8c23
wanglintong <309101041@qq.com>
2017-03-09 09:55:41 +0800

    Update MainActivity.java

--- app/src/main/java/cn/com/zlqf/safe/MainActivity.java
+++ app/src/main/java/cn/com/zlqf/safe/MainActivity.java
@@ -2,7 +2,8 @@ package cn.com.zlqf.safe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/**2017 3.9
+*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
